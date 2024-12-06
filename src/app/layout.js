efe5bc7980342b8d4b195f8e656ddc6d8bb5e755
D. Resnyanskiy <resnyanskiy05@gmail.com>
2024-12-06 21:21:45 +0500

    Added navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import NavBar from "@/components/NavBar";
 
 export const metadata = {
 	title: 'Create Next App',
@@ -7,7 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html>
-			<body>{children}</body>
+			<body>
+			{children}
+			</body>
 		</html>
 	);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import LoginForm from "@/components/LoginForm";
 import RegisterForm from "@/components/RegisterForm";
 import NewDebtForm from "@/components/NewDebtForm";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
 	return <div className={"home-page"}>
+		<NavBar isAuth={true}></NavBar>
 		<RegisterForm></RegisterForm>
 	</div>;
 }
