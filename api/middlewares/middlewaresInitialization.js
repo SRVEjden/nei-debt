8dfba42c24da0886342031911fef18c05bc99ec3
Roman Savich <romasavich2002@gmail.com>
2024-12-07 20:25:30 +0300

    Work with environment variables

--- api/middlewares/middlewaresInitialization.js
+++ api/middlewares/middlewaresInitialization.js
@@ -5,10 +5,10 @@ import passport from 'passport';
 const router = express.Router();
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
-router.use(cookieParser('HelloWorld'));
+router.use(cookieParser(process.env.SECRET_COOKIE));
 router.use(
 	session({
-		secret: 'secretWord',
+		secret: process.env.SECRET_SESSION,
 		saveUninitialized: false,
 		resave: false,
 		cookie: {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from 'express';
 import next from 'next';
 import { middlewaresInitialization } from './api/middlewares/middlewaresInitialization.js';
