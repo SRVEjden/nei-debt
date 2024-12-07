9c8aba05ab6a19c9275115f3c8349e0fe373f884
Roman Savich <romasavich2002@gmail.com>
2024-12-07 20:17:05 +0300

    a little refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui");
+import daisyui from 'daisyui';
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [
-      daisyui,
-  ],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: [daisyui],
 };
